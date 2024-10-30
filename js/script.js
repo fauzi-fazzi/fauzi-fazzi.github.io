ee4c3ef4c5221f632da8d01a0c25dd7d97421fac
fazzi <ahmadfauzi.fazzi@gmail.com>
2024-10-30 23:54:41 +0700

    edit cv

--- js/script.js
+++ js/script.js
@@ -1,34 +1,35 @@
 //hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', function(){
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
 
 // navbar fixed
-window.onscroll = function(){
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
 
-    if(window.pageYOffset > fixedNav){
-        header.classList.add('navbar-fixed');
-    }else{
-        header.classList.remove('navbar-fixed')
-    }
-}
-
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
 
 // pesan whatsapp
 function kirimKeWhatsApp(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const nama = document.getElementById("name").value;
-    const pesan = document.getElementById("pesan").value;
-    
-    const nomorWhatsApp = "6282138145768"; // Gunakan format 62 untuk nomor Indonesia
-    const url = `https://wa.me/${nomorWhatsApp}?text=Halo, saya ${nama}. Pesan saya: ${encodeURIComponent(pesan)}`;
+  const nama = document.getElementById("name").value;
+  const pesan = document.getElementById("pesan").value;
 
-    window.open(url, '_blank');
+  const nomorWhatsApp = "6282138145768"; // Gunakan format 62 untuk nomor Indonesia
+  const url = `https://wa.me/${nomorWhatsApp}?text=Halo, saya ${nama}. Pesan saya: ${encodeURIComponent(
+    pesan
+  )}`;
+
+  window.open(url, "_blank");
 }
