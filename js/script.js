eebd0a512413533808db94995f216b7cf518f4c5
fazzi <ahmadfauzi.fazzi@gmail.com>
2024-10-30 11:05:33 +0700

    form-update

--- js/script.js
+++ js/script.js
@@ -18,3 +18,17 @@ window.onscroll = function(){
         header.classList.remove('navbar-fixed')
     }
 }
+
+
+// pesan whatsapp
+function kirimKeWhatsApp(event) {
+    event.preventDefault();
+
+    const nama = document.getElementById("name").value;
+    const pesan = document.getElementById("pesan").value;
+    
+    const nomorWhatsApp = "6282138145768"; // Gunakan format 62 untuk nomor Indonesia
+    const url = `https://wa.me/${nomorWhatsApp}?text=Halo, saya ${nama}. Pesan saya: ${encodeURIComponent(pesan)}`;
+
+    window.open(url, '_blank');
+}
